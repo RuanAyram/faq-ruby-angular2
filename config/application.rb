1ce61a15da784cfbd3ce2a7badebfa4f7651fe51
Ruan <ruan_kaylo@yahoo.com.br>
2019-08-31 15:47:01 -0300

    Adjustments

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Faq
         config.middleware.insert_before 0, Rack::Cors do
             allow do
                 origins '*'
-                resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
+                resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put]
             end
         end
         # Settings in config/environments/* take precedence over those specified here.
